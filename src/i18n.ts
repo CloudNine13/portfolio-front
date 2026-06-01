6b425ef3d226c020fd096d9fd94fc96eb4acb015
Igor Dzichkovskii <Igordzich@gmail.com>
2026-06-01 19:43:29 +0200

    Initial commit

---
+++ src/i18n.ts
@@ -0,0 +1,38 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslation from './locales/en/translation.json';
+import esTranslation from './locales/es/translation.json';
+import ruTranslation from './locales/ru/translation.json';
+
+export const defaultNS = 'translation';
+export const resources = {
+  en: {
+    translation: enTranslation,
+  },
+  es: {
+    translation: esTranslation,
+  },
+  ru: {
+    translation: ruTranslation,
+  },
+} as const;
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    defaultNS,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
